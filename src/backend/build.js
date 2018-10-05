301cc66813468d826916a6b5e2c45b9295e1eb2a
Canopy JS <canopyjs@gmail.com>
2018-10-05 11:12:17 +0300

    Add backend superdirectory & use commandline arguments

---
+++ src/backend/build.js
@@ -0,0 +1,12 @@
+var generateJsonFilesFromDgsDirectory = require('./generate_json_files_from_dgs_directory.js');
+
+if (process.argv.length < 4) {
+  console.log('Example Usage:')
+  console.log('node src/backend/build.js /Users/Me/project/topics /Users/Me/project/build')
+  throw('Script requires source and destination path as command line arguments')
+}
+
+generateJsonFilesFromDgsDirectory(
+  process.argv[2],
+  process.argv[3]
+);
--- src/build.js
+++
@@ -1,7 +0,0 @@
-var generateJsonFilesFromDgsDirectory = require('./generate_json_files_from_dgs_directory.js');
-
-// Todo: make these command line arguments
-generateJsonFilesFromDgsDirectory(
-  '/Users/Allen/cheshbonot/topics',
-  '/Users/Allen/cheshbonot/build'
-);
