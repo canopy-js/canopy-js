9c8cf553695e3789240456d7fa547841f55f52df
Canopy JS <canopyjs@gmail.com>
2018-10-18 20:55:18 +0300

    Add css to project

--- src/frontend/canopy.js
+++ src/frontend/canopy.js
@@ -2,6 +2,7 @@ import { canopyContainer, defaultTopic } from 'helpers/getters';
 import displayTopic from 'display/display_topic';
 import { topicNameFromUrl, subtopicNameFromUrl } from 'helpers/url_parsers'
 import setPathAndFragment from 'helpers/set_path_and_fragment';
+import css from 'style/canopy.css';
 
 if (!topicNameFromUrl()) {
   setPathAndFragment(defaultTopic, null);
