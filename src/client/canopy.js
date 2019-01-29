dafd90c63f289bf1506449417d4592ce53a6bd35
Canopy JS <canopyjs@gmail.com>
2019-01-29 20:21:14 +0200

    Add Element#prepend with dom-shims

--- src/client/canopy.js
+++ src/client/canopy.js
@@ -3,6 +3,7 @@ import updateView from 'display/update_view';
 import registerKeyListeners from 'keys/register_key_listeners';
 import registerPopStateListener from 'history/register_pop_state_listener';
 import parsePathString from 'path/parse_path_string';
+import 'dom-shims';
 
 registerKeyListeners();
 registerPopStateListener();
