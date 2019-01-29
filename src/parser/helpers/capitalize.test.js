50582626e021bb1a4a45f02188b4c98e529e0c6d
Canopy JS <canopyjs@gmail.com>
2019-01-29 20:22:28 +0200

    Add case testing capitalization of string in quotes

--- src/parser/helpers/capitalize.test.js
+++ src/parser/helpers/capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from './capitalize';
 test('it capitalizes a word', () => {
   expect(capitalize('america')).toBe('America');
 });
+
+test('it capitalizes a word in quotes', () => {
+  expect(capitalize('"america"')).toBe('"America"');
+});
