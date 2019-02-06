28f212edeb64b964536c882877895c395f944847
Canopy JS <canopyjs@gmail.com>
2019-02-06 19:00:20 +0200

    Find shorter regex for clausesWithPunctuationOf

--- src/parser/helpers/clauses_with_punctuation_of.js
+++ src/parser/helpers/clauses_with_punctuation_of.js
@@ -6,31 +6,27 @@ function clausesWithPunctuationOf(string) {
 //
 //  [.,:;?!] = clause terminating punctuation
 //  ["'()<>{}[\]] = wrapping punctuation
-//  node.js = one word, two word segments
 //
 //  Regex:
 //
-//  /
-//    (
-//      (?:                    Match a series of word segments
-//        \s*                  which may begin with whitespace.
-//        (?:
-//          [.,:;?!]           Words may begin with clause terminating punctuation,
-//          |
-//          ["'()<>{}[\]]      or wrapping punctuation.
-//        )*
-//        \w+                  There must be at least one word character per word.
-//        ["'()<>{}[\]]*       Words can end with wrapping punctuation, but not clause-terminating punctuation.
-//        \s*                  A space can separate one word from the next, or a word from subsequent clause termination.
+//    /
+//      (
+//         .                    // Match one or more characters
+//        (?!                   // that aren't followed by
+//          [.,:;?!]+           // clause-terminal punctuation
+//          ["'()<>{}[\]]*      // followed by optional wrapping punctuation
+//          (\s|$)              // followed by a space or end of string.
+//        )
 //      )+
+//      .                       // Match the last character before the punctuation,
+//      [.,:;?!]+               // match the punctuation itself,
+//      ["'()<>{}[\]]?          // and match match any wrapping punctuation that follows it.
+//    /g
 //
-//     [.,:;?!]+               If a word is reached that _does_ end in clause separating punctuation, terminate the clause.
-//     [\"\'()<>{}[\]]*        Any wrapping punctuation that follows the clause-termination is included in the clause.
-//   )
-// /g
+//
 
   return Array.from(
-    string.match(/((?:\s*(?:[.,:;?!]|["'()<>{}[\]])*\w+["'()<>{}[\]]*\s*)+[.,:;?!]+[\"\'()<>{}[\]]*)/g)
+    string.match(/(.(?![.,:;?!]+["'()<>{}[\]]*(\s|$)))+.[.,:;?!]+["'()<>{}[\]]?/g)
   );
 }
 
