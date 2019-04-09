bf8f4542be7901fe41948c3ddce7118bb3176ea6
CanopyJS <canopyjs@gmail.com>
2019-04-09 15:42:09 +0300

    Make new repo for docs

--- script/docs_deploy.sh
+++ script/docs_deploy.sh
@@ -16,7 +16,7 @@ git init
 git config user.name "CanopyJS"
 git config user.email "canopyjs@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/canopy-js/canopy-js.git"
+git remote add upstream "https://$GH_TOKEN@github.com/canopy-js/canopy-js.github.io.git"
 git fetch upstream
 git reset upstream/gh-pages
 
