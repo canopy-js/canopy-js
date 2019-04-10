e3a29103f249a6c2e115d8c92086ac1fdce10ac2
CanopyJS <canopyjs@gmail.com>
2019-04-10 16:15:12 +0300

    Try to commit build to build branch

---
+++ script/code_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd dist
+
+git init
+git config user.name "CanopyJS"
+git config user.email "canopyjs@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/canopy-js/canopy-js.git"
+git fetch upstream
+git reset upstream/build
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:build
