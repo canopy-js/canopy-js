fae2464d406f0409a74edf1590e70eccd9489094
CanopyJS <canopyjs@gmail.com>
2019-04-11 13:25:41 +0300

    Use travis.yml to limit builds by branch

--- script/docs_deploy.sh
+++ script/docs_deploy.sh
@@ -2,12 +2,6 @@
 set -x
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master!"
-  exit 0
-fi
-
 rev=$(git rev-parse --short HEAD)
 
 cd docs/build
