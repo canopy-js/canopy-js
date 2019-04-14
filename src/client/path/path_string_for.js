c46fa8022ec7d8aca4e81d4ce756dcdc49f4cfbc
CanopyJS <canopyjs@gmail.com>
2019-04-14 12:54:27 +0300

    Refix path string when pathPrefix is null

--- src/client/path/path_string_for.js
+++ src/client/path/path_string_for.js
@@ -2,10 +2,11 @@ import { slugFor } from 'helpers/identifiers';
 import { pathPrefix } from 'helpers/getters';
 
 function pathStringFor(pathArray) {
-  return pathPrefix + '/' + pathArray.map((tuple) => {
-    return slugFor(tuple[0]) +
-      (tuple[1] && tuple[1] !== tuple[0] ? ('#' + slugFor(tuple[1])) : '')
-  }).join('/');
+  return (pathPrefix ? '/' + pathPrefix : '') +
+    '/' + pathArray.map((tuple) => {
+      return slugFor(tuple[0]) +
+        (tuple[1] && tuple[1] !== tuple[0] ? ('#' + slugFor(tuple[1])) : '');
+    }).join('/');
 }
 
 export default pathStringFor;
