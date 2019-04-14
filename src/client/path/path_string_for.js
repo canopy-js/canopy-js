1fe7ebd5cb44a3735b3842d43a3c510a1d15c54d
CanopyJS <canopyjs@gmail.com>
2019-04-14 12:36:30 +0300

    Revert "Make trailing slash in path default"
    This reverts commit 8d3888be861c72f2360b7823af168b09e356f552.

--- src/client/path/path_string_for.js
+++ src/client/path/path_string_for.js
@@ -2,11 +2,10 @@ import { slugFor } from 'helpers/identifiers';
 import { pathPrefix } from 'helpers/getters';
 
 function pathStringFor(pathArray) {
-  return (pathPrefix ? '/' + pathPrefix : '') +
-    '/' + pathArray.map((tuple) => {
-      return slugFor(tuple[0]) +
-        (tuple[1] && tuple[1] !== tuple[0] ? ('#' + slugFor(tuple[1])) : '');
-    }).join('/') + '/';
+  return pathPrefix + '/' + pathArray.map((tuple) => {
+    return slugFor(tuple[0]) +
+      (tuple[1] && tuple[1] !== tuple[0] ? ('#' + slugFor(tuple[1])) : '')
+  }).join('/');
 }
 
 export default pathStringFor;
