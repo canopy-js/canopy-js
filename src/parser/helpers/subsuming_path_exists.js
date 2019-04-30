1b3c67dae684424edc7f1efaefc7cbd95c33a13f
CanopyJS <canopyjs@gmail.com>
2019-04-30 16:59:23 -0400

    Add cycle detection for localReferenceGraph

--- src/parser/helpers/subsuming_path_exists.js
+++ src/parser/helpers/subsuming_path_exists.js
@@ -1,16 +1,20 @@
-function subsumingPathExists(topic1, topic2, localReferenceGraph) {
+function subsumingPathExists(topic1, topic2, localReferenceGraph, stack=[]) {
   if (topic1 === topic2) {
     return true;
   } else if (!localReferenceGraph.hasOwnProperty(topic1)) {
     return false;
   } else if (localReferenceGraph[topic1].includes(topic2)) {
     return true;
+  } else if (stack.includes(topic1)) {
+    return false // reject cycles
   } else {
+    stack.push(topic1);
     return localReferenceGraph[topic1].some(
       (referencedSubtopic) => subsumingPathExists(
         referencedSubtopic,
         topic2,
-        localReferenceGraph
+        localReferenceGraph,
+        stack
       )
     )
   }
