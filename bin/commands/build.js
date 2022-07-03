3ddac640e833bd87d2784abaf34055d18ba7eb2c
Canopy JS <canopyjs@gmail.com>
2022-07-03 12:57:45 +0000

    Code built at c9f7189

--- bin/commands/build.js
+++ bin/commands/build.js
@@ -4,8 +4,8 @@ const child_process = require('child_process');
 const buildProject = require('./build/build_project');
 
 function build({ symlinks, projectPathPrefix, hashUrls }) {
-	if (!fs.existsSync('./topics')) throw 'There must be a topics directory present, try running "canopy init"';
-	if (!fs.existsSync('./.canopy_default_topic')) throw 'There must be a default topic dotfile present, try running "canopy init"';
+	if (!fs.existsSync('./Topics')) console.error('There must be a topics directory present, try running "canopy init"') || process.exit();
+	if (!fs.existsSync('./.canopy_default_topic')) console.error('There must be a default topic dotfile present, try running "canopy init"') || process.exit();
 
 	let defaultTopic = fs.readFileSync('.canopy_default_topic').toString().trim();
 	canopyLocation = child_process.execSync("echo ${CANOPY_LOCATION:-$(readlink -f $(which canopy) | xargs dirname | xargs dirname)}").toString().trim();
