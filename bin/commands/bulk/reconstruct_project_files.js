bfcbc260f5b9186b69d4408830cb395622f96470
Canopy JS <canopyjs@gmail.com>
2022-07-13 09:17:19 -0400

    Handle uncategorized notes before asterisk in bulk mode
    The bulk mode file is supposed to look like this:
    ```
      [A/B/C]
    
      * Topic1: ABC.
    
      Subtopic1: DEF.
    
      * Topic2: ABC.
    
      Subtopic2: DEF.
     ```
    
    It begins with a path, and then has an asterisk for each file.
    
    However, what if the user enters a first line without an asterisk?
    ```
      [A/C]
    
      Notes without asterisk.
    
      * Topic3: ABC.
     ```
    
     These notes have no file to be stored under, and so are now interpreted
     as belonging to the "category file", ie A/C/C.expl. If that is a real
     file, they will be appended to the file.

--- bin/commands/bulk/reconstruct_project_files.js
+++ bin/commands/bulk/reconstruct_project_files.js
@@ -2,15 +2,71 @@ let fs = require('fs-extra');
 let { keyFromString } = require('./helpers');
 let { TopicName, Paragraph } = require('../shared');
 
+/*
+
+  A canopy bulk file might look like this:
+
+  -- File begins --
+  [A/B/C]
+
+  * Topic1: ABC.
+
+  Subtopic1: DEF.
+
+  * Topic2: ABC.
+
+  Subtopic2: DEF.
+
+
+  [A/C]
+
+  Notes without asterisk.
+
+  * Topic3: ABC.
+
+  * File without key.
+
+  -- File ends --
+
+  This file should produce the following files and folders:
+
+  - topics/A/B/C/Topic1.expl:
+    Topic1: ABC.
+
+    Subtopic1: DEF.
+
+  - topics/A/B/C/Topic2.expl:
+    Topic2: ABC.
+
+    Subtopic2: DEF.
+
+  - topics/A/C/Topic3.expl:
+    Topic3: ABC.
+
+  - topics/A/C/C.expl:
+    Notes without asterisk.
+
+    File without key.
+
+*/
+
 function reconstructProjectFiles(dataFile, originalFileList) {
   let pathHandled = {};
-  let sections = dataFile.split(/(?=^\[.*\]\n)/gm);
+  let sections = dataFile.split(/(?=^\[.*\]\n)/gm); // Take the path-initial sections
 
   sections.filter(Boolean).forEach(section => {
-    let path = section.split("\n")[0].match(/\[(.*)\]/)[1];
+    let blocks = section.split("\n\n");
+    let path = blocks[0].match(/\[(.*)\]/)[1]; //
     let pathSegments = path.split('/');
-    let fileTexts = section.split(/(?=^\* )/gm).slice(1).map(file => file.slice(2));
+    let fileTexts = blocks.slice(1).join("\n\n").split(/(?=^\* )/gm); // Take the
     let categoryNotes = [];
+
+    if (!fileTexts[0].startsWith("* ")) { // Notes without asterisk before first asterisk
+      categoryNotes.push(fileTexts[0]) // Keep a list of uncategorized notes to put in A/B/C/C.expl eg.
+      fileTexts = fileTexts.slice(1);
+    }
+
+    fileTexts = fileTexts.map(text => text.slice(2)) // remove "* " before file contents
     let directoriesToEnsure = [];
     let filesToWrite = {};
 
@@ -36,12 +92,13 @@ function reconstructProjectFiles(dataFile, originalFileList) {
       }
     });
 
-    let categoryNotePath = `topics/${path}/${pathSegments.slice(-1)[0]}.expl`;
+    let categoryName = pathSegments.slice(-1)[0];
+    let categoryNotePath = `topics/${path}/${categoryName}.expl`;
     if (categoryNotes.length > 0) {
-      if (filesToWrite[categoryNotePath]) {
+      if (filesToWrite[categoryNotePath]) { // If there is a real file already existing at A/B/C/C.expl, add to it
         filesToWrite[categoryNotePath] = filesToWrite[categoryNotePath] + "\n\n" + categoryNotes.join("\n\n");
       } else {
-        filesToWrite[categoryNotePath] = categoryNotes.join("\n\n");
+        filesToWrite[categoryNotePath] = categoryNotes.join("\n\n"); // Otherwise start a new one
       }
     }
 
