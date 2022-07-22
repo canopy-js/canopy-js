4dcce57670b2c292b13a9aa8e74e7f2a159c6a12
Canopy JS <canopyjs@gmail.com>
2022-07-22 11:53:58 -0400

    Allow `canopy serve PORT` syntax
    This commit introduces a shorthand for running the server at a
    particular port.
    
    This commit also removes the unused mode argument to canopy watch –
    the watch command used to take a start/stop argument, but now the user
    is responsible for managing the watch process.

--- bin/commands/watch.js
+++ bin/commands/watch.js
@@ -3,7 +3,7 @@ const child_process = require('child_process');
 const build = require('./build');
 const chokidar = require('chokidar');
 
-function watch(mode, options) {
+function watch(options) {
 	let canopyLocation = child_process
 		.execSync("echo ${CANOPY_LOCATION:-$(readlink -f $(which canopy) | xargs dirname | xargs dirname)}")
 		.toString().trim();
