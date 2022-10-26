a705373d17d3f219a87cdbd728b38911216892ab
Canopy JS <canopyjs@gmail.com>
2022-10-26 18:56:02 -0400

    Add `canopy bulk --sync` command and improve other logging
    Now running `canopy bulk --sync` will generate a `canopy_bulk_file`
    that can be edited repeatedly to rebuild the topic directory and
    by extension the JSON build files.
    
    This commit also improves the log messages of the watch and new sync
    command to include simpler time stamps and a mention of the specific
    file changed, and adds colors to other logging.

--- cli/commands/watch.js
+++ cli/commands/watch.js
@@ -3,6 +3,7 @@ const path = require('path');
 const build = require('./build');
 const chokidar = require('chokidar');
 const shell = require('shelljs');
+let chalk = require('chalk');
 
 function watch(options) {
   let canopyLocation = process.env.CANOPY_LOCATION || path.dirname(path.dirname(fs.realpathSync(shell.which('canopy').stdout)));
@@ -12,19 +13,28 @@ function watch(options) {
     return;
   }
 
-  buildWrapper(options);
+  if (!options.suppressInitialBuild || (options.buildIfUnbuilt && !fs.existsSync('build/_data'))) {
+    buildWrapper(options);
+  }
 
   const watcher = chokidar.watch(['topics', `${canopyLocation}/dist`], { persistent: true, ignoreInitial: true });
+
+  let debounceTimer;
+  const debounce = (callback, time) => {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(callback, time);
+  }
+
   watcher.on('add', (e) => {
-    buildWrapper({...options, ...{ filesEdited: e }});
+    debounce(() => buildWrapper({...options, ...{ filesEdited: e }}), 500);
   }).on('addDir', (e) => {
-    buildWrapper({...options, ...{ filesEdited: e }});
+    debounce(() => buildWrapper({...options, ...{ filesEdited: e }}), 500);
   }).on('change', (e) => {
-    buildWrapper({...options, ...{ filesEdited: e }});
+    debounce(() => buildWrapper({...options, ...{ filesEdited: e }}), 500);
   }).on('unlink', (e) => {
-    buildWrapper({...options, ...{ filesEdited: e }});
+    debounce(() => buildWrapper({...options, ...{ filesEdited: e }}), 500);
   }).on('unlinkDir', (e) => {
-    buildWrapper({...options, ...{ filesEdited: e }});
+    debounce(() => buildWrapper({...options, ...{ filesEdited: e }}), 500);
   })
 }
 
@@ -32,8 +42,9 @@ function buildWrapper(options) {
   try {
     build(options);
   } catch (e) {
-    console.error(`Canopy watch process pid ${process.pid} failed to build topic files`);
-    console.error(e.message);
+    console.error(chalk.bgRed(chalk.black(`Canopy watch process (pid ${process.pid}) failed to build topic files`)));
+    if (options.sync) fs.writeFileSync(options.bulkFileName, fs.readFileSync(options.bulkFileName).toString() + `\n// ${e.message}`);
+    console.error(e);
   }
 }
 
