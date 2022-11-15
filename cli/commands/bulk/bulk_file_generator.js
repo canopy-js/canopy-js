f05b79c1fc337683c9326c2e3f6f73504ce0f818
Canopy JS <canopyjs@gmail.com>
2022-11-15 09:24:31 -0500

    Object-oriented refactor of bulk mode code
    Previously, the bulk mode file generation and processing involved many
    add-hoc functions that took a large number of different data structures
    representing different file information. This refactor replaces the
    ad-hoc functions with a series of classes with single responsibilities,
    and replaces the data structures with FileSets which encapsulate many
    facts about a set of files and directories which previously were
    represented using multiple different objects and arrays.

---
+++ cli/commands/bulk/bulk_file_generator.js
@@ -0,0 +1,49 @@
+let Paragraph = require('../shared/paragraph');
+
+class BulkFileGenerator {
+  constructor(fileSet, defaultTopicDisplayCategoryPath, defaultTopicFilePath) {
+    this.fileSet = fileSet;
+    this.directoryComparator = generateDirectoryComparator(defaultTopicDisplayCategoryPath);
+    this.fileComparator = generateFileComparator(defaultTopicFilePath);
+  }
+
+  generateBulkFile() {
+    return this.fileSet.directories.sort(this.directoryComparator).filter(d => d.files.length > 0).map(directory => {
+      return `[${directory.displayPath}]\n\n`
+        + directory
+            .files
+            .sort(this.fileComparator)
+            .filter(f => f.path.endsWith('.expl'))
+            .map(file =>
+              (!file.categoryNotes && (new Paragraph(file.contents)).key ? '* ' : '')
+              + file.contents.trim() // trim trailing newlines to ensure spacing is consistent
+            ).join('\n\n\n'); // three newlines between files
+
+    }).join('\n\n\n') + '\n\n\n'; // three newlines between the last file and the next category, and three at the end for space when adding
+  }
+}
+
+function generateDirectoryComparator(defaultTopicDisplayCategoryPath) {
+  return function directoryComparator(directory1, directory2) {
+    if (directory1.displayPath === 'Inbox') return 1;
+    if (directory2.displayPath === 'Inbox') return -1;
+
+    if (directory1.displayPath === defaultTopicDisplayCategoryPath) return -1;
+    if (directory2.displayPath === defaultTopicDisplayCategoryPath) return 1;
+
+    if (directory1.displayPath > directory2.displayPath) return 1;
+    if (directory1.displayPath < directory2.displayPath) return -1;
+  }
+}
+
+function generateFileComparator(defaultTopicFilePath) {
+  return function fileComparator(file1, file2) {
+    if (file1.path === defaultTopicFilePath) return -1;
+    if (file2.path === defaultTopicFilePath) return 1;
+
+    if (file1.path > file2.path) return 1;
+    if (file1.path < file2.path) return -1;
+  }
+}
+
+module.exports = BulkFileGenerator;
