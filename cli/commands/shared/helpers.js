40300a02f4f0247faa876c85f431553c1ddb86c6
Canopy JS <canopyjs@gmail.com>
2022-11-16 10:32:44 -0500

    Change default topic file to path and add validation
    Previously, the `.canopy_default_topic` file held the key of the
    default topic, rather than the path to the file, but then checking
    whether that topic existed was more difficult, and so now we store the
    path, and throw an error if the default topic path doesn't correspond
    to a real file. In bulk mode, we validate on ending a session and log an
    error if there is no longer a valid default topic file.
    
    The `.canopy_default_topic` file is now just `canopy_default_topic`
    and not a dotfile, because the user should be aware of it.

--- cli/commands/shared/helpers.js
+++ cli/commands/shared/helpers.js
@@ -2,13 +2,12 @@ let fs = require('fs');
 let Topic = require('./topic');
 let Paragraph = require('./paragraph');
 let { getRecursiveSubdirectoryFiles } = require('../bulk/helpers');
+let chalk = require('chalk');
 
 function getDefaultTopicAndPath() {
-  let defaultTopicName = fs.readFileSync('.canopy_default_topic').toString().trim();
-  let defaultTopicFilePath = getRecursiveSubdirectoryFiles('topics').find(path => {
-    let paragraph = new Paragraph(fs.readFileSync(path).toString());
-    return paragraph.key === defaultTopicName;
-  });
+  let defaultTopicFilePath = fs.readFileSync('canopy_default_topic').toString().trim();
+  if (!fs.existsSync(defaultTopicFilePath)) throw new Error(chalk.bgRed(`Default topic file does not exist: ${defaultTopicFilePath}`));
+  let defaultTopicName = (new Paragraph(fs.readFileSync(defaultTopicFilePath).toString().trim())).key
   let categoryPath = defaultTopicFilePath?.match(/topics\/(.*)\/[^\/]+.expl/)[1];
   let defaultTopicDisplayCategoryPath = categoryPath && Topic.convertUnderscoresToSpaces(categoryPath);
   return {
