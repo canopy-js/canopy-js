18e0ec195bdc55a25d064b7ec90581d5063fb2e2
Canopy JS <canopyjs@gmail.com>
2022-11-16 11:20:29 -0500

    Make error messages red
    And update tests to expect it.

--- cli/commands/serve/serve.js
+++ cli/commands/serve/serve.js
@@ -11,7 +11,7 @@ function serve(options) {
 
   let validBuild = ['build', 'build/index.html', 'build/_data', 'build/canopy.js'].map(s => fs.existsSync(s)).every(Boolean);
   if (!validBuild) {
-    throw new Error(`Error: Cannot start server because build directory is invalid, try running \`canopy build\` and check for errors`);
+    throw new Error(chalk.red(`Error: Cannot start server because build directory is invalid, try running \`canopy build\` and check for errors`));
   }
 
   if (options.logging) console.log(`Serving on port ${port}`);
