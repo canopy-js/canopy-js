fc0f35cbaf075e70e0382d6951a0191e6c22481e
Canopy JS <canopyjs@gmail.com>
2022-12-04 12:07:18 -0500

    Extract canopyLocation into helper
    Previously the shell command would be run in every location the value
    was needed, now we just compute the constant once and refer to it.

--- cli/commands/shared/helpers.js
+++ cli/commands/shared/helpers.js
@@ -3,12 +3,14 @@ let Topic = require('./topic');
 let Paragraph = require('./paragraph');
 let { getRecursiveSubdirectoryFiles } = require('../bulk/helpers');
 let chalk = require('chalk');
+let path = require('path');
+let shell = require('shelljs')
 
 function getDefaultTopicAndPath() {
   if (!fs.existsSync('canopy_default_topic')) throw new Error('There must be a default topic file present, try running "canopy init"');
   let defaultTopicFilePath = fs.readFileSync('canopy_default_topic').toString().trim();
   if (!defaultTopicFilePath || !fs.existsSync(defaultTopicFilePath)) {
-    throw new Error(chalk.red(`Error: No topic file corresponds to name given in canopy_default_topic: "${defaultTopicName.trim()}"`));
+    throw new Error(chalk.red(`Error: No topic file corresponds to the path given in canopy_default_topic: ${defaultTopicFilePath.trim()}`));
   }
   if (!fs.existsSync(defaultTopicFilePath)) throw new Error(chalk.bgRed(`Default topic file does not exist: ${defaultTopicFilePath}`));
   let defaultTopicName = (new Paragraph(fs.readFileSync(defaultTopicFilePath).toString().trim())).key
@@ -22,4 +24,6 @@ function getDefaultTopicAndPath() {
   }
 }
 
-module.exports = { getDefaultTopicAndPath };
+let canopyLocation = process.env.CANOPY_LOCATION || path.dirname(path.dirname(fs.realpathSync(shell.which('canopy').stdout)));
+
+module.exports = { getDefaultTopicAndPath, canopyLocation };
