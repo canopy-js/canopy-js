eb32089fcfcec5b926faaa97d1066bfb4af4bde6
Canopy JS <canopyjs@gmail.com>
2024-01-24 17:39:35 +0000

    Playground built at 27dbf81

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "resolver": "jest-webpack-resolver",
-  "moduleNameMapper": {
-    '\\.svg$': '<rootDir>/playwright/mock_svg.js'
-  }
-}
--- playwright/mock_svg.js
+++
@@ -1,1 +0,0 @@
-module.exports = 'mocked-svg';
