78c84feb862fcd8cfcf72c6ddd3aa6d5826cb182
Canopy JS <canopyjs@gmail.com>
2024-02-13 13:55:01 -0500

    Delete erroneous link selection metadata
    If we look for a link that does not exist, rather than erroring,
    just remove the record for next time.

--- playwright/style.spec.js
+++ playwright/style.spec.js
@@ -6,6 +6,14 @@ test.beforeEach(async ({ page }) => {
       console.error(message.text());
     }
   })
+
+  await page.goto('/United_States');
+  await expect(page).toHaveURL("United_States");
+  await page.evaluate(() => localStorage.clear()); // get rid of old link selections
+  await page.evaluate(() => sessionStorage.clear()); // get rid of old link selections
+  await page.waitForFunction(() => {
+    return localStorage.length === 0 && sessionStorage.length === 0;
+  });
 });
 
 test.describe('Text styles', () => {
