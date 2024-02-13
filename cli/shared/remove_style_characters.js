ea7d69a667b12ad48d7ab7a52959eee664f8888e
Canopy JS <canopyjs@gmail.com>
2024-02-13 11:41:09 -0500

    Add client performance features
    In conjunction with the previously committed `--cache` option that
    improves support for large data sets, this commit introduces several
    performance-related features on the client side.
    
    First, a bug is fixed which caused eager rendering to target links
    that were not visible. Now, links will have a `renderOnly`
    request initiated when they become visible.
    
    Secondly, on each path change, we now "prune" the DOM, removing
    any paragraph that is not visible or targeted by a visible link.
    
    Lastly, pure functions like removeStyleTokens or Topic#constructor
    that are heavily used are now cached to save time.

--- cli/shared/remove_style_characters.js
+++ cli/shared/remove_style_characters.js
@@ -5,11 +5,16 @@ In order to remove multiple layers of wrapping, eg *_a_*, each call removes the 
   If there was no difference, then we have run out of style tokens and can return.
 */
 
+let Cache = {};
+
 function removeStyleCharacters(string) {
+  if (Cache.hasOwnProperty(string)) return Cache[string];
   let newString = string.replace(/(.*?(?:^|[^\\]))([_`*~])(\S*?[^\\])\2(.*)/, '$1$3$4'); // style characters within words if no spaces
   newString = newString.replace(/(.*?(?:^|[^\\]))([_`*~])(.*?[^\\])\2(?![A-Za-z0-9])(.*)/, '$1$3$4'); // style characters at edges regardless of content
   if (newString !== string) {
-    return removeStyleCharacters(newString);
+    let processedString = removeStyleCharacters(newString);
+    Cache[string] = processedString;
+    return removeStyleCharacters(processedString);
   } else {
     return string;
   }
