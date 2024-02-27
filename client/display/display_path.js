c4a894fe2dff58a8e25ff5b12ee5c05918d446a9
Canopy JS <canopyjs@gmail.com>
2024-02-27 14:37:56 -0500

    Remove noDisplay option
    This option was introduced in order to do link persisting in history
    without changing the DOM, but this can be done by just calling the
    helper directly rather that introducing further complexity into
    displayPath.

--- client/display/display_path.js
+++ client/display/display_path.js
@@ -19,22 +19,19 @@ function displayPath(pathToDisplay, linkToSelect, options = {}) {
   if (linkToSelect && !pathToDisplay.includes(linkToSelect.enclosingPath)) throw 'linkToSelect argument is not on given pathToDisplay';
   if (!Paragraph.byPath(pathToDisplay)) return tryPathPrefix(pathToDisplay, options);
 
+  resetDom(pathToDisplay);
+  Paragraph.byPath(pathToDisplay).addToDom(); // add before reset so classes on DOM elements are removed
+  try { linkToSelect?.element } catch { linkToSelect.eraseLinkData(); return updateView(pathToDisplay, null, options); }
   Path.setPath(linkToSelect?.urlPath || pathToDisplay, options); // must be done before link.select because selection cache is by current URL
   Link.persistLinkSelection(linkToSelect); // if null, persists deselect
-  if (options.noDisplay) return;
-
-  Paragraph.byPath(pathToDisplay).addToDom(); // add before reset so classes on DOM elements are removed
-  resetDom(pathToDisplay);
   Link.updateSelectionClass(linkToSelect); // if null, removes previous selection's class
   let header = setHeader(pathToDisplay.rootTopicPath.topic, options);
   document.title = pathToDisplay.lastTopic.mixedCase;
 
-  try { linkToSelect?.element } catch { linkToSelect.eraseLinkData(); return updateView(pathToDisplay, null, options); }
   let visibleParagraphs = displayPathTo(pathToDisplay.paragraph, [], options);
   pathToDisplay.paragraph.addSelectionClass();
   setTimeout(() => visibleParagraphs.forEach(paragraph => paragraph.display()) || header.show());
   setTimeout(() => Link.visible.filter(link => link.isGlobal).forEach(link => setTimeout(() => link.execute({ renderOnly: true })))); // eager render
-  // setTimeout(() => Paragraph.pruneDom()); // Reintroduce if necessary due to DOM size loading issues
   return options.noScroll ? Promise.resolve() : scrollPage(linkToSelect, options);
 };
 
