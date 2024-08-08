575734c26ec4bf1814e626e80de96ca31444578c
Canopy JS <canopyjs@gmail.com>
2024-08-07 20:26:39 -0400

    Add default topic JSON and css to build html
    It seems that having a larger initial index asset with fewer blocking
    requests is more efficient on commercially available hosting services.
    
    This change adds the default topic's JSON to the index.html file in a
    JSON script tag, and then adds it to the request cache on page load.

--- client/requests/request_cache.js
+++ client/requests/request_cache.js
@@ -1,3 +1,9 @@
+import { defaultTopic, defaultTopicJson } from 'helpers/getters';
+
 let REQUEST_CACHE = {};
+
+if (defaultTopicJson()) {
+  REQUEST_CACHE[defaultTopic()] = Promise.resolve(JSON.parse(defaultTopicJson()));
+}
 
 export default REQUEST_CACHE;
