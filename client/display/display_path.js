8a36226bbb732aa0e9a5461df6b007c393bcc428
Canopy JS <canopyjs@gmail.com>
2024-10-30 14:41:22 -0400

    Indicate scroll preference with optional link argument
    UpdateView has formal parameters of a path and link argument. However,
    never are both required. By passing a link, we can determine from its
    type what path should be visible when it is selected, unless it is an
    inlined cycle link, in which case by passing the inline cycle path we
    clarify the link's open state.
    
    Previously, we had populated the missing argument from the present one,
    either in the caller or in the function body eg first line of
    displayPath. However, semantically this obfuscated whether the caller
    itself had specified eg a link argument, or whether the path was
    the significant component.
    
    This commit also removes the concept of scrollToParagraph as an
    argument, replacing it with the aforementioned presence or non-presence
    of linkToSelect as the representation of the caller's intention to
    scroll to link or paragraph.
    
    A scrollToParagraph argument is preserved in order to allow link#execute
    to remain initiator-neutral (i.e. keys vs. mouse), however, the flag
    is just a wrapper for link#select to defer to path#display.

--- client/display/display_path.js
+++ client/display/display_path.js
@@ -13,15 +13,14 @@ import {
 import { canopyContainer } from 'helpers/getters';
 
 function displayPath(pathToDisplay, linkToSelect, options = {}) {
-  if (!linkToSelect) linkToSelect = pathToDisplay.paragraph?.parentLink; // always select link?
   if (!Paragraph.byPath(pathToDisplay)) return tryPathPrefix(pathToDisplay, options);
   beforeChangeScroll(pathToDisplay, linkToSelect, options).then(() => {  // eg long distance up or two-step path transition
     Paragraph.byPath(pathToDisplay).addToDom(); // add before reset so classes on DOM elements are removed
     resetDom(pathToDisplay);
     if (linkToSelect && !linkToSelect?.element) { linkToSelect?.eraseLinkData(); return updateView(pathToDisplay, null, options); }
     Path.setPath(linkToSelect?.urlPath || pathToDisplay, options); // before link.select because selection cache by current URL
-    Link.persistLinkSelection(linkToSelect); // if null, persists deselect
-    Link.updateSelectionClass(linkToSelect); // if null, removes previous selection's class
+    Link.persistLinkSelection(linkToSelect || pathToDisplay.parentLink); // if null, persists deselect
+    Link.updateSelectionClass(linkToSelect || pathToDisplay.parentLink); // if null, removes previous selection's class
     let header = setHeader(pathToDisplay.rootTopicPath.topic, options);
     document.title = pathToDisplay.lastTopic.mixedCase;
 
