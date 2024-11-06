4bb22a6887c5e11677f39cfedca12739fb43f916
Canopy JS <canopyjs@gmail.com>
2024-11-06 16:53:38 -0500

    Tweak parameters for table auto-standardization

--- client/render/render_token_element.js
+++ client/render/render_token_element.js
@@ -513,9 +513,6 @@ function renderList(listNodeObjects, renderContext) {
 }
 
 function renderTable(token, renderContext) {
-  const WIDTH_STANDARDIZE_MIN = 60;
-  const HEIGHT_STANDARDIZE_MIN = 50;
-
   let tableElement = document.createElement('TABLE');
   tableElement.setAttribute('dir', 'auto');
   if (token.rtl) tableElement.setAttribute('dir', 'rtl');
@@ -568,8 +565,11 @@ function renderTable(token, renderContext) {
   canopyContainer.appendChild(tempSectionElement);
   tempParagraphElement.appendChild(tableElement);
 
+  const WIDTH_STANDARDIZE_MIN = 100;
+  const HEIGHT_STANDARDIZE_MIN = 50;
+
   let sizes = {widest: -1, narrowest: Infinity, tallest: -1, shortest: Infinity};
-  tempParagraphElement.appendChild(tableElement);
+
   [...tableElement.querySelectorAll('td')].forEach(tableCellElement => {
     let clsp = tableCellElement.getAttribute('colspan');
     let rwsp = tableCellElement.getAttribute('rowspan');
@@ -579,13 +579,21 @@ function renderTable(token, renderContext) {
     if (!rwsp && tableCellElement.offsetHeight < sizes.shortest) sizes.shortest = tableCellElement.offsetHeight;  
   });
 
-  if (sizes.widest - sizes.narrowest < WIDTH_STANDARDIZE_MIN) {
+  if (sizes.widest - sizes.narrowest <= WIDTH_STANDARDIZE_MIN) {
     [...tableElement.querySelectorAll('td')].forEach(td => {td.style.width = sizes.widest + 'px'; })
   }
+  tableElement.dataset.widest = sizes.widest;
+  tableElement.dataset.narrowest = sizes.narrowest;
+  tableElement.dataset.widthDiff = sizes.widest - sizes.narrowest;
+  tableElement.dataset.standardize_width_min = WIDTH_STANDARDIZE_MIN;
 
-  if (sizes.tallest - sizes.shortest < HEIGHT_STANDARDIZE_MIN) {
+  if (sizes.tallest - sizes.shortest <= HEIGHT_STANDARDIZE_MIN) {
     [...tableElement.querySelectorAll('td')].forEach(td => {td.style.height = sizes.tallest + 'px'; })
   }
+  tableElement.dataset.tallest = sizes.tallest;
+  tableElement.dataset.shortest = sizes.shortest;
+  tableElement.dataset.heightDiff = sizes.tallest - sizes.shortest;
+  tableElement.dataset.standardize_height_min = HEIGHT_STANDARDIZE_MIN;
 
   canopyContainer.removeChild(tempSectionElement);
   tempParagraphElement.removeChild(tableElement);
