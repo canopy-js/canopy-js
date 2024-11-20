aed03c649d5e5aa14994b83cb735789045fd1f99
Canopy JS <canopyjs@gmail.com>
2024-11-20 10:37:17 -0500

    Use spans for link icons rather than ::after blocks
    This change transitions from using ::after blocks for various post-link
    icons to using regular spans.
    
    This was motivated by a desire to improve the accuracy of menu sizing
    which has great difficulty detecting and estimating the size of ::after
    blocks. However, the change also improves the legibility of tests,
    asserting on visible text, and simplifies the DOM structure in general.

--- client/render/helpers.js
+++ client/render/helpers.js
@@ -98,4 +98,38 @@ function whenInDom(element) {
   };
 }
 
-export { generateHeader, measureVerticalOverflow, whenInDom };
+function getCombinedBoundingRect(element) { // taking into consideration relative positioning which native function doesn't do
+  // Initialize the bounds using the element's bounding rect
+  let rect = element.getBoundingClientRect();
+  let combinedRect = { top: rect.top, left: rect.left, bottom: rect.bottom, right: rect.right };
+
+  // Recursively look at all the children
+  Array.from(element.children).forEach(child => {
+    // Get the bounding rect of the child
+    let childRect = child.getBoundingClientRect();
+    
+    // Adjust combined rect to include the child's bounding box
+    combinedRect.top = Math.min(combinedRect.top, childRect.top);
+    combinedRect.left = Math.min(combinedRect.left, childRect.left);
+    combinedRect.bottom = Math.max(combinedRect.bottom, childRect.bottom);
+    combinedRect.right = Math.max(combinedRect.right, childRect.right);
+
+    // Recursively include the child's children
+    let childCombinedRect = getCombinedBoundingRect(child);
+    combinedRect.top = Math.min(combinedRect.top, childCombinedRect.top);
+    combinedRect.left = Math.min(combinedRect.left, childCombinedRect.left);
+    combinedRect.bottom = Math.max(combinedRect.bottom, childCombinedRect.bottom);
+    combinedRect.right = Math.max(combinedRect.right, childCombinedRect.right);
+  });
+
+  // Calculate the width and height of the combined bounding box
+  combinedRect.width = combinedRect.right - combinedRect.left;
+  combinedRect.height = combinedRect.bottom - combinedRect.top;
+
+  // Draw the bounding box visually around the final outer bounding box
+  // drawBoundingBox(combinedRect);
+
+  return combinedRect;
+}
+
+export { generateHeader, measureVerticalOverflow, whenInDom, getCombinedBoundingRect };
