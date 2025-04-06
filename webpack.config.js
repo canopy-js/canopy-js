603b73a0cd850169486466a379110fac566be94a
Canopy JS <canopyjs@gmail.com>
2025-04-06 13:58:38 +0000

    Code built at d6dd1b5

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
-
-class MovePlaygroundPlugin { // need this to happen after build creates playground.js asset
-  apply(compiler) {
-    compiler.hooks.emit.tapAsync('MovePlaygroundPlugin', (compilation, callback) => {
-      const asset = compilation.assets['playground.js'];
-      if (asset) {
-        compilation.assets['playground/playground.js'] = asset;
-      }
-      callback();
-    });
-  }
-}
 
 module.exports = {
   entry: {
     canopy: './client/canopy.js',
-    playground: './playground/src.js',
+    'playground/playground': './playground/src.js',
     rebuild_canopy: './client/rebuild_canopy.js'
   },
   output: {
@@ -32,8 +19,7 @@ module.exports = {
       path.resolve(__dirname, 'client/'),
       'node_modules'
     ],
-    extensions: [ '.js' ],
-
+    extensions: ['.js'],
     fallback: { "path": require.resolve("path-browserify") }
   },
   module: {
@@ -41,13 +27,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: { loader: "babel-loader" }
       },
       {
-        test:/\.(s*)css$/,
-        use:['style-loader','css-loader', 'sass-loader']
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.svg$/,
@@ -55,22 +39,23 @@ module.exports = {
       }
     ]
   },
-
   stats: { colors: true },
   target: 'web',
   devtool: false,
   plugins: [
     new webpack.SourceMapDevToolPlugin({
-      filename: "_[file].map"
+      filename: '_[name].js.map'
     }),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'playground'),
-          to: path.resolve(__dirname, 'dist/playground')
+          to: path.resolve(__dirname, 'dist/playground'),
+          globOptions: {
+            ignore: ['**/src.js']
+          }
         }
       ]
-    }),
-    new MovePlaygroundPlugin()
+    })
   ]
 }
