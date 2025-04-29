3834b75d04f69bda05d921c46cd876258200061c
Canopy JS <canopyjs@gmail.com>
2025-04-29 15:26:58 -0400

    Extract getTopicCategories helper for note and utility commands

--- cli/utility.js
+++ cli/utility.js
@@ -1,21 +1,10 @@
-let { execSync } = require('child_process');
-let path = require('path');
 let Topic = require('./shared/topic');
+let { getCategoryPathStrings } = require('./shared/fs-helpers');
 
 function utility(options = {}) {
-
   if (options.categories) {
-    let topicsDir = path.join(process.cwd(), 'topics');
     try {
-      let output = execSync(`find "${topicsDir}" -type d`, { encoding: 'utf8' });
-      let lines = output
-        .split('\n')
-        .map(p => p.trim())
-        .filter(p => p && p !== topicsDir)
-        .map(p => path.relative(topicsDir, p))
-        .map(rel => rel.split(path.sep).join('/'))
-        .sort();
-
+      let lines = getCategoryPathStrings();
       for (let line of lines) {
         console.log(Topic.convertUnderscoresToSpaces(line));
       }
