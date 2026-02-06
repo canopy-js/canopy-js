b5830b09c450690283caa4667ceb659ec40939e1
Canopy JS <canopyjs@gmail.com>
2026-02-06 10:09:05 -0500

    Block quotes should leave terminal \n to create subsequent padding
    Elements are responsible for creating their upper padding, so we need
    the \n to become part of the lower text to create a gap.
    
    A blockquote that ends a paragraph will leave a \n which in CSS we now
    ignore.

--- cli/build/components/matchers.js
+++ cli/build/components/matchers.js
@@ -95,12 +95,12 @@ function blockQuoteMatcher({ string, parserContext, startOfLine }) {
       .map(m => m?.[1] || '')
       .join('\n');
 
-    if (text[text.length - 1] === "\n") text = text.slice(0, -1); // remove trailing newline
     let direction = match[2] === '>' ? 'ltr' : 'rtl';
 
+    const matchLength = match[0].endsWith('\n') ? match[0].length - 1 : match[0].length; // leave terminal newline for next token
     return [
       new BlockQuoteToken(text, direction, parserContext),
-      match[0].length
+      matchLength
     ];
   }
 }
